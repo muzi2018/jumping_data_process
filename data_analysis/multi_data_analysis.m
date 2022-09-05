601dc5ca767aeb00cc4f02e1a4ab95cef64fa3f8
Weijie wang <3127870356@qq.com>
2022-09-06 06:15:37 +0800

    pitch moment

--- data_analysis/multi_data_analysis.m
+++ data_analysis/multi_data_analysis.m
@@ -343,4 +343,121 @@ close all; clear;clc;
 
 
 %% Stability force
+load('h=0.1_l=0.1/v_nas.mat');
+Mp_NAS_1=Velocity_Recording.pitch_moment;
+Mp_NAS_1=Mp_NAS_1(:,2:end);
+load('h=0.1_l=0.1/v_as.mat');
+Mp_AS_1=Velocity_Recording.pitch_moment;
+Mp_AS_1=Mp_AS_1(:,2:end);
+
+
+load('h=0.1_l=0.15/v_nas.mat');
+Mp_NAS_2=Velocity_Recording.pitch_moment;
+Mp_NAS_2=Mp_NAS_2(:,2:end);
+load('h=0.1_l=0.15/v_as.mat');
+Mp_AS_2=Velocity_Recording.pitch_moment;
+Mp_AS_2=Mp_AS_2(:,2:end);
+
+
+load('h=0.15_l=0.1/v_nas.mat');
+Mp_NAS_3=Velocity_Recording.pitch_moment;
+Mp_NAS_3=Mp_NAS_3(:,2:end);
+load('h=0.15_l=0.1/v_as.mat');
+Mp_AS_3=Velocity_Recording.pitch_moment;
+Mp_AS_3=Mp_AS_3(:,2:end);
+
+
+load('h=0.15_l=0.15/v_nas.mat');
+Mp_NAS_4=Velocity_Recording.pitch_moment;
+Mp_NAS_4=Mp_NAS_4(:,2:end);
+load('h=0.15_l=0.15/v_as.mat');
+Mp_AS_4=Velocity_Recording.pitch_moment;
+Mp_AS_4=Mp_AS_4(:,2:end);
+
+
+load('h=0.2_l=0.1/v_nas.mat');
+Mp_NAS_5=Velocity_Recording.pitch_moment;
+Mp_NAS_5=Mp_NAS_5(:,2:end);
+load('h=0.2_l=0.1/v_as.mat');
+Mp_AS_5=Velocity_Recording.pitch_moment;
+Mp_AS_5=Mp_AS_5(:,2:end);
+
+
+load('h=0.2_l=0.15/v_nas.mat');
+Mp_NAS_6=Velocity_Recording.pitch_moment;
+Mp_NAS_6=Mp_NAS_6(:,2:end);
+load('h=0.2_l=0.15/v_as.mat');
+Mp_AS_6=Velocity_Recording.pitch_moment;
+Mp_AS_6=Mp_AS_6(:,2:end);
+
+time=Velocity_Recording.lcm_timestamp/5;
+a=200+300;
+num=5000;
+time=time(:,a:num);
+
+f1=figure;
+% subplot(3,1,1);
+% plot(time,Mp_NAS_1(:,a:num),'sb--','MarkerIndices',1:100:length(time), 'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_AS_1(:,a:num),'sr--','MarkerIndices',1:100:length(time), 'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_NAS_2(:,a:num),'db--','MarkerIndices',1:100:length(time),'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_AS_2(:,a:num),'dr--','MarkerIndices',1:100:length(time),'LineWidth',1.2);
+% hold on;
+% xlabel('time (s)');ylabel('moment (N·m·s)');
+% xlim([0.5 6]);
+% set(gca, 'linewidth',1,'FontSize',15,'Fontname', 'Times New Roman','FontWeight','bold');
+% legend('h=0.1\_l=0.1\_NAS','h=0.1\_l=0.1\_AS',...
+%         'h=0.1\_l=0.15\_NAS','h=0.1\_l=0.15\_AS',...
+%         'Location', 'northeast');
+
+    
+    
+% time=Velocity_Recording.lcm_timestamp/5;
+% a=200+359;
+% num=4999;
+% time=time(:,a:num);
+% subplot(3,1,2);
+% plot(time,Mp_NAS_3(:,a:num),'^b--','MarkerIndices',1:100:length(time), 'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_AS_3(:,a:num),'^r--','MarkerIndices',1:100:length(time), 'LineWidth',1.2);
+% hold on;
+% time=Velocity_Recording.lcm_timestamp/5;
+% a=200+363;
+% num=4999;
+% time=time(:,a:num);
+% plot(time,Mp_NAS_4(:,a:num),'vb--','MarkerIndices',1:100:length(time),'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_AS_4(:,a:num),'vr--','MarkerIndices',1:100:length(time),'LineWidth',1.2);
+% hold on;
+% xlabel('time (s)');ylabel('moment (N·m·s)');
+% xlim([0.563 6]);
+% set(gca, 'linewidth',1,'FontSize',15,'Fontname', 'Times New Roman','FontWeight','bold');
+% legend('h=0.15\_l=0.1\_NAS','h=0.15\_l=0.1\_AS',...
+%         'h=0.15\_l=0.15\_NAS','h=0.15\_l=0.15\_AS',...
+%         'Location', 'northeast');
+
+    
+    
+% time=Velocity_Recording.lcm_timestamp/5;
+% a=200+430;
+% num=4999;
+% time=time(:,a:num);
+% subplot(3,1,3);
+% plot(time,Mp_NAS_5(:,a:num),'<b--','MarkerIndices',1:100:length(time), 'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_AS_5(:,a:num),'<r--','MarkerIndices',1:100:length(time), 'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_NAS_6(:,a:num),'>b--','MarkerIndices',1:100:length(time),'LineWidth',1.2);
+% hold on;
+% plot(time,Mp_AS_6(:,a:num),'>r--','MarkerIndices',1:100:length(time),'LineWidth',1.2);
+% hold on;
+% xlabel('time (s)');ylabel('moment (N·m·s)');
+% xlim([0.63 6]);
+% set(gca, 'linewidth',1,'FontSize',15,'Fontname', 'Times New Roman','FontWeight','bold');
+% legend('h=0.2\_l=0.1\_NAS','h=0.2\_l=0.1\_AS',...
+%         'h=0.2\_l=0.15\_NAS','h=0.2\_l=0.15\_AS',...
+%         'Location', 'northeast');
+
 a=1;
